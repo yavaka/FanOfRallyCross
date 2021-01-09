149f99ec4c0dd80a42e4bef6e8b1caaa078edf15
yavaka <yavakasolutions@gmail.com>
2021-01-09 15:21:04 +0000

    Add drivers overlay and their skeleton pages.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+function openDriversOverlay() {
+    document.getElementById("drivers").style.height = "100%";
+}
 
-
-
+function closeDriversOverlay() {
+    document.getElementById("drivers").style.height = "0%";
+}
