33b184334e28289df8a17026150edbda48847e53
yavaka <yavakasolutions@gmail.com>
2021-01-25 22:48:58 +0000

    Add code comments

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,39 @@ const details = "details";
 const gallery = "gallery";
 const video = "video";
 
+// After one of the drivers page is loaded, 
+// the details button is disabled and the details sub-page is loaded.
 $(document).ready(function () {
     document.getElementById("driver-details").style.display = "block";
     document.getElementById("details-btn").disabled = true;
 });
 
+// Open drivers overlay
 function openDriversOverlay() {
     document.getElementById("drivers").style.height = "100%";
     document.body.style.overflow = "hidden";
     document.body.style.height = "100%";
 }
 
+// Close drivers overlay
 function closeDriversOverlay() {
     document.getElementById("drivers").style.height = "0%";
     document.body.style.overflow = "auto";
     document.body.style.height = "auto";
 }
 
+// Show driver details
 function showDriverDetails() {
+    // invoke hide function and pass details constant
     hide(details);
 
+    // disable the details btn 
     document.getElementById("details-btn").disabled = true;
 
+    // get drivers details sub-page
     var driverDetails = document.getElementById("driver-details");
 
+    // check is the sub-page loaded, if true hide it, if false load it
     if (driverDetails.style.display === "block") {
         driverDetails.style.display = "none";
     }
@@ -34,13 +43,18 @@ function showDriverDetails() {
     }
 }
 
+// Show driver gallery
 function showDriverGallery() {
+    // invoke hide function and pass gallery constant
     hide(gallery);
 
+    // disable the gallery btn 
     document.getElementById("gallery-btn").disabled = true;
 
+    // get drivers gallery sub-page
     var driverGallery = document.getElementById("driver-gallery");
 
+    // check is the sub-page loaded, if true hide it, if false load it
     if (driverGallery.style.display === "block") {
         driverGallery.style.display = "none";
     }
@@ -49,13 +63,18 @@ function showDriverGallery() {
     }
 }
 
+// Show driver video
 function showDriverVideo() {
+    // invoke hide function and pass video constant
     hide(video);
 
+    // disable the video btn 
     document.getElementById("video-btn").disabled = true;
 
+    // get drivers video sub-page
     var driverVideo = document.getElementById("driver-video");
 
+    // check is the sub-page loaded, if true hide it, if false load it
     if (driverVideo.style.display === "block") {
         driverVideo.style.display = "none";
     }
@@ -64,6 +83,7 @@ function showDriverVideo() {
     }
 }
 
+// If the parameter is details will hide gallery and video sub-pages, etc for the rest
 function hide(current) {
     switch (current) {
         case details:
@@ -83,37 +103,49 @@ function hide(current) {
     }
 }
 
+// Hide details sub-page
 function hideDetails() {
+    // remove disabled attribute of the btn
     if (document.getElementById("details-btn").disabled === true) {
         document.getElementById("details-btn").disabled = false;
     }
 
+    // get drivers details sub-page
     var driverDetails = document.getElementById("driver-details");
 
+    // hide the sub-page if it is rendered
     if (driverDetails.style.display === "block") {
         driverDetails.style.display = "none";
     }
 }
 
+// Hide gallery sub-page
 function hideGallery() {
+    // remove disabled attribute of the btn
     if (document.getElementById("gallery-btn").disabled === true) {
         document.getElementById("gallery-btn").disabled = false;
     }
 
+    // get drivers gallery sub-page
     var driverGallery = document.getElementById("driver-gallery");
 
+    // hide the sub-page if it is rendered
     if (driverGallery.style.display === "block") {
         driverGallery.style.display = "none";
     }
 }
 
+// Hide video sub-page
 function hideVideo() {
+    // remove disabled attribute of the btn
     if (document.getElementById("video-btn").disabled === true) {
         document.getElementById("video-btn").disabled = false;
     }
 
+    // get drivers video sub-page
     var driverVideo = document.getElementById("driver-video");
 
+    // hide the sub-page if it is rendered
     if (driverVideo.style.display === "block") {
         driverVideo.style.display = "none";
     }
@@ -124,6 +156,7 @@ $('.navbar-nav>li>a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
 
+// Validation of the contact form
 function validateForm() {
     var fullName = document.forms["contact-form"]["full-name"].value;
     var subject = document.forms["contact-form"]["subject"].value;
